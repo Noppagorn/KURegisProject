92f6e7b3b54544c521c9934841c357a8818d7f25
NoppaGorn <tongnop2540@gmail.com>
2018-11-20 13:09:27 +0700

    Can create Table view

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -3,11 +3,25 @@ package sample;
 import Combine.ComputerScience;
 import Student.AllStudent;
 import Subject.Subject;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
 
 import java.util.ArrayList;
 
 public class Controller {
+
+    @FXML
+    TableColumn<Subject,String> idSubject;
+    @FXML
+    TableView<Subject> tableSubject;
+
+    @FXML
+    TableColumn<Subject,String> nameSubTable;
+    @FXML TableColumn<Subject,String> BaseTable;
     private ArrayList<Subject> subjects = null;
     public Controller(){
         ComputerScience computerScience = new ComputerScience();
@@ -16,13 +30,29 @@ public class Controller {
     }
     @FXML
     public void initialize(){
-        ShowAll();
+        showAllID();
+        showAllName();
+        showAllBase();
     }
-    private void ShowAll(){
-        for (Subject x: subjects
-             ) {
-            System.out.println(x);
-        }
+
+    private void showAllID(){
+        //subjects = new ArrayList<>();
+        ObservableList<Subject> subjectsObs = FXCollections.observableArrayList(this.subjects);
+        tableSubject.setItems(subjectsObs);
+        idSubject.setCellValueFactory(param -> new SimpleStringProperty(param.getValue().getCode()));
     }
+
+    private void showAllName(){
+        ObservableList<Subject> subjectsObs = FXCollections.observableArrayList(this.subjects);
+        tableSubject.setItems(subjectsObs);
+        nameSubTable.setCellValueFactory(param -> new SimpleStringProperty(param.getValue().getName()));
+    }
+
+    private void showAllBase(){
+        ObservableList<Subject> subjectsObs = FXCollections.observableArrayList(this.subjects);
+        tableSubject.setItems(subjectsObs);
+        BaseTable.setCellValueFactory(param -> new SimpleStringProperty(Integer.toString(param.getValue().getWeight())));
+    }
+
 
 }
